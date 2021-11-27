9afa450a6f7feae1df3a8d4e476bbc47cb0357d2
osama <osama.w.hafez@gmail.com>
2021-11-27 15:00:52 -0500

    fix https settings, add lessons route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const lesson = require("./routes/lesson");
 
 var port = 3001;
 
@@ -10,34 +11,35 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use("/api", lesson);
 
 app.use((req, res, next) => {
-  console.log(`\nEndpoint Hit: ${req.originalUrl}\n`);
-  next();
+    console.log(`\nEndpoint Hit: ${req.originalUrl}\n`);
+    next();
 });
 
 if (process.env.NODE_ENV == "production") {
-  // This sets the options for https so that it finds the ssl certificates
-  var privateKey = fs.readFileSync(
-    "/etc/letsencrypt/live/offlinequran.org-0001/privkey.pem"
-  );
-  var certificate = fs.readFileSync(
-    "/etc/letsencrypt/live/offlinequran.org-0001/cert.pem"
-  );
-  var chain = fs.readFileSync(
-    "/etc/letsencrypt/live/offlinequran.org-0001/fullchain.pem"
-  );
-  const httpsOptions = {
-    cert: certificate,
-    key: privateKey,
-    ca: chain,
-  };
+    // This sets the options for https so that it finds the ssl certificates
+    var privateKey = fs.readFileSync(
+        "/etc/letsencrypt/live/offlinequran.org-0001/privkey.pem"
+    );
+    var certificate = fs.readFileSync(
+        "/etc/letsencrypt/live/offlinequran.org-0001/cert.pem"
+    );
+    var chain = fs.readFileSync(
+        "/etc/letsencrypt/live/offlinequran.org-0001/fullchain.pem"
+    );
+    const httpsOptions = {
+        cert: certificate,
+        key: privateKey,
+        ca: chain,
+    };
 
-  var httpsServer = https.createServer(httpsOptions, app).listen(port, () => {
-    console.log("Serving on https");
-  });
+    var httpsServer = https.createServer(httpsOptions, app).listen(port, () => {
+        console.log("Serving on https");
+    });
 } else if (process.env.NODE_ENV == "development") {
-  app.listen(port, () => {
-    console.log("Listening on port " + port);
-  });
+    app.listen(port, () => {
+        console.log("Listening on port " + port);
+    });
 }
