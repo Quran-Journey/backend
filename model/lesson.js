412cf92edccee759ff21f406c08a2c56d0fca6df
osama <osama.w.hafez@gmail.com>
2021-11-29 01:31:09 -0500

    initial model and route with documentation example

---
+++ model/lesson.js
@@ -0,0 +1,52 @@
+const c = require("./constants");
+
+/**
+ *  @schema AllLessons
+ *  type: array
+ *  items:
+ *      $ref: '#/definitions/Lesson'
+ *  tags: Falafel
+ */
+async function getLessons() {
+    return await c.retrieve("SELECT * FROM Lesson;");
+}
+
+/**
+ *  @schema Lesson
+ *  type: object
+ *  required:
+ *      - lesson_number
+ *      - lesson_date
+ *      - source
+ *  properties:
+ *      lesson_number: integer
+ *      lesson_date: string
+ *      source: string
+ *      audio_file: string
+ *  tags: Falafel
+ */
+async function createLesson(data) {
+    // Frontend note: also add a feature where we guess that the
+    //  lesson's date is the next saturday after the last lesson's date
+    var invalid = c.simpleValidation(data, {
+        lesson_number: "string",
+        lesson_date: "date",
+        source: "string",
+    });
+    if (invalid) {
+        return invalid;
+    }
+    var sql =
+        "INSERT INTO lesson (lesson_number, source, lesson_date) VALUES ($1, $2, $3) RETURNING *;";
+    var params = [data.lesson_number, data.source, data.lesson_date];
+    return await c.create(
+        sql,
+        params,
+        new c.Message({ success: "Successfully created a lesson." })
+    );
+}
+
+module.exports = {
+    getLessons: getLessons,
+    createLesson: createLesson,
+};
---
+++ routes/lesson.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const l = require("../model/lesson");
+const c = require("./routingConstants");
+
+/*
+ * @api [get] /lessons
+ *  summary: "Returns all lessons from the system."
+ *  tags:
+ *    - lesson
+ *  responses:
+ *    200:
+ *      description: A list of lessons.
+ *      response:
+ *        - $ref: '#/components/schemas/AllLessons'
+ *    404:
+ *      description: No lessons found.
+ *
+ */
+router.get("/lessons", (request, response) => {
+    await l.getLessons(request.body).then(async function (result) {
+        return c.simpleResponse(result, response);
+    });
+});
+
+/*
+ * @api [get] /lessons
+ *  summary: "Returns all lessons from the system."
+ *  tags:
+ *    - lesson
+ *  parameters:
+ *        - $ref: '#/components/AllLessons'
+ *  responses:
+ *    200:
+ *      description: A list of lessons.
+ *      response:
+ *        - $ref: '#/components/schemas/AllLessons'
+ *    404:
+ *      description: No lessons found.
+ *
+ */
+router.get("/lessons", (request, response) => {
+  await l.createLesson(request.body).then(async function (result) {
+      return c.simpleResponse(result, response);
+  });
+});
+
+module.exports = router;
