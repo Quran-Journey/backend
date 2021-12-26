d61ed8702479b6dcabb402761926c5a384f95ca2
osama <osama.w.hafez@gmail.com>
2021-12-25 23:41:05 -0500

    clean up test run script

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,8 @@
 # We will use a test db server on firebase.
-# cd db; sh repop.sh > ../testOutput.txt; cd ..;
-cd db; docker-compose up -d > ../testOutput.txt; cd ..;
+# rm testOutput.txt
+cd db; docker-compose up -d > ../testOutput.txt; cd ..; sleep 5;
 node index.js >> ./testOutput.txt 2>> ./testOutput.txt &
 backend=$!
 npm test
-cd db docker-compose down; cd ..;
 kill $backend
+cd db; docker-compose down 2>> ../testOutput.txt; cd ..;
