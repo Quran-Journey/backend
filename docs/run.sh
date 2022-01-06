e233e40586dd33b15a12e8d7589e17437c4dc732
osama <osama.w.hafez@gmail.com>
2022-01-06 07:36:33 -0500

    separate processing from serving

--- docs/run.sh
+++ docs/run.sh
@@ -2,5 +2,3 @@ npx swagger-inline "../model/*.js" "../routes/*.js" --base oasBase.json --format
 python3 formatOAS.py temp.json
 rm temp.json
 redoc-cli bundle oas.json -t doc.hbs --options.theme.colors.primary.main=brown
-docker build . -t qj_docs
-docker run -p 80:80 qj_docs
