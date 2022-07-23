8f5eea684d6d50bce00f8c9de31a55ec1081ec6b
osama <osama.w.hafez@gmail.com>
2022-07-23 16:44:48 -0400

    add python and bash scripts along with data sheets from roots repo

---
+++ db/backup.sh
@@ -0,0 +1,13 @@
+# Comment out this part of the script if you are running things locally
+docker-compose down;
+docker-compose up --build -d;
+
+# Uncomment this part of the script if you are running postgres locally
+# PGPASSWORD=postgres psql quranjourney < drop.sql;
+# PGPASSWORD=postgres psql quranjourney < quran-simple.sql;
+# PGPASSWORD=postgres psql quranjourney < root_tables.sql;
+
+python3 insert_data.py;
+cd ../model; node insertMeanings.js;
+
+docker exec -i db_postgres_alone_1 /bin/bash -c "PGPASSWORD=Yatathakar123! pg_dump --username qj quranJourney" > ./filled.sql
