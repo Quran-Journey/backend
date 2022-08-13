0d073843c0595701567810c20470c20fb346bd8d
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2022-08-13 14:13:29 -0400

    Created 4 api requests for Surahs

--- model/surah.js
+++ model/surah.js
@@ -32,24 +32,47 @@ const attributes = {
  *          example: "https://www.facebook.com/watch/live/?ref=watch_permalink&v=244235014324418"
  */
 
-/* TO DO -- CREATE SURAH  */ 
+
+/** Create a Surah, requires all attributes of the Surah except surah_id which is generated as PK (need to verify this) */
 async function createSurah(data) {
-    // Frontend note: also add a feature where we guess that the
-    //  lesson's date is the next saturday after the last lesson's date
     var invalid = utils.simpleValidation(data, {
-        lesson_date: "date",
-        source: "string",
+        surah_number: "date",
+        revelation_place: "string",
+        name_complex: "string",
+        name_arabic: "string",
+        verse_count: "integer",
     });
     if (invalid) {
         return invalid;
     }
     var sql =
-        "INSERT INTO Lesson (source, lesson_date) VALUES ($1, $2) RETURNING *;";
-    var params = [data.source, data.lesson_date];
+        "INSERT INTO Surah (surah_number, revelation_place, name_complex, name_arabic, verse_count) VALUES ($1, $2, $3, $4, $5) RETURNING *;";
+    var params = [data.surah_number, data.revelation_place, data.name_complex, data.name_arabic, data.verse_count];
     return await utils.create(
         sql,
         params,
         new utils.Message({ success: "Successfully created a lesson." })
+    );
+}
+
+
+/** Fetches Surahs based on a specific filter (i.e. surah_id, surah_number) */
+async function getSurahById(data) {
+    var invalid = utils.simpleValidation(data, {
+        surah_id: "integer",
+    });
+    if (invalid) {
+        return invalid;
+    }
+    let sql = "SELECT * FROM Surah WHERE surah_id=$1";
+    var params = [data.surah_id];
+    return await utils.retrieve(
+        sql,
+        params,
+        new utils.Message({
+            success: `Successfully fetched Surah with id ${data.surah_id}.`,
+            none: `Could not find a Surah with id ${data.surah_id}.`,
+        })
     );
 }
 
@@ -63,10 +86,6 @@ async function updateSurah(data) {
         name_complex: "string",
         name_arabic: "string",
         verse_count: "integer",
-        
-        surah_id: "integer",
-        lesson_date: "date",
-        source: "string",
     });
     if (invalid) {
         return invalid;
@@ -104,9 +123,8 @@ async function deleteSurah(data) {
 }
 
 module.exports = {
-    filterLessons: filterLessons,
-    getLessonById: getLessonById,
-    createLesson: createLesson,
+    getSurahById: getSurahById,
+    createSurah: createSurah,
     updateSurah: updateSurah,
     deleteSurah: deleteSurah,
 };
