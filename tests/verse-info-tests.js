1495aad4eb1e8d8782705cd464db2b8e863085da
osama <osama.w.hafez@gmail.com>
2022-08-23 08:49:49 -0400

    rewrite verse info tests to be more thorough

--- tests/verse-info-tests.js
+++ tests/verse-info-tests.js
@@ -8,22 +8,29 @@ function verseInfoTests() {
         let reflectionInfo = seedData.Reflection[0];
         let tafsirInfo = seedData.Tafsir[0];
         let arabicWord = seedData.ArabicWord[0];
-        let verseWord = seedData.VerseWord[0]
+        let verseWord = seedData.VerseWord[0];
 
         const resp1 = await apiGET(`/verse/1`);
-        expect(resp1.data.data.reflections[0]).toEqual(reflectionInfo)
-        expect(resp1.data.data.reflections.length).toEqual(2)
+        expect(resp1.data.data.reflections[0]).toEqual(reflectionInfo);
+        expect(resp1.data.data.reflections.length).toEqual(2);
 
-        checkTafsirMatch(resp1.data.data.tafsirs[0], tafsirInfo)
-        checkRootMatch(resp1.data.data.roots[0], verseWord, arabicWord)
+        checkTafsirMatch(resp1.data.data.tafsirs[0], tafsirInfo);
+        checkWordMatch(resp1.data.data.words[0], verseWord, arabicWord);
         expect(resp1.data.success).toEqual(true);
-    })
-    it("get complete verse info (negative test)", async () => {
-        const resp2 = await apiGET(`/verse/2`)
-        expect(resp2.data.data.reflections.length).toEqual(0);
-        expect(resp2.data.success).toEqual(false);
-
-    })
+    });
+    it("get verse info with only verse explanation", async () => {
+        let arabicWord = seedData.ArabicWord[2];
+        let verseWord = seedData.VerseWord[1];
+        const resp = await apiGET(`/verse/2`);
+        expect(resp.data.data.reflections.length).toEqual(0);
+        checkWordMatch(resp.data.data.words[0], verseWord, arabicWord);
+        expect(resp.data.success).toEqual(true);
+    });
+    it("get verse info for a verse that does not exist", async () => {
+        const resp = await apiGET(`/verse/3`);
+        expect(resp.data.ecode).toEqual(3);
+        expect(resp.data.success).toEqual(false);
+    });
 }
 
 function checkTafsirMatch(t1, t2) {
@@ -31,14 +38,15 @@ function checkTafsirMatch(t1, t2) {
     expect(t1.content).toEqual(t2.content);
     expect(t1.verse_id).toEqual(t2.verse_id);
 }
-function checkRootMatch(t1, vw, aw) {
-    expect(t1.root_id).toEqual(aw.root_id)
-    expect(t1.verse_id).toEqual(vw.verse_id)
-    expect(t1.word_id).toEqual(vw.word_id)
-    expect(t1.word).toEqual(aw.word)
+function checkWordMatch(t1, vw, aw) {
+    expect(t1.root_id).toEqual(aw.root_id);
+    expect(t1.verse_id).toEqual(vw.verse_id);
+    expect(t1.word_id).toEqual(vw.word_id);
+    expect(t1.word).toEqual(aw.word);
+    expect(t1.visible).toEqual(vw.visible);
+    expect(t1.word_explanation).toEqual(vw.word_explanation);
 }
 
 module.exports = {
     verseInfoTests: verseInfoTests,
 };
-
