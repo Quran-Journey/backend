3150c7477a3580c3154b5644a53be38ccac60543
osama <osama.w.hafez@gmail.com>
2022-08-28 19:33:43 -0400

    fix documentation and functionality for lesson related functions

--- model/lesson.js
+++ model/lesson.js
@@ -1,5 +1,5 @@
 const utils = require("./utils");
-const verseInfo = require("./verseInfo")
+const verseInfo = require("./verseInfo");
 
 // Note: this list contains key value pairs of the attribute and types within the schema.
 const attributes = {
@@ -44,50 +44,40 @@ const attributes = {
  *  @schema LessonContent
  *  type: object
  *  required:
- *      - lessonInfo
- *      - lesson
+ *      - lesson_content
+ *      - lesson_id
+ *      - lesson_date
+ *      - start_verse
+ *      - end_verse
+ *      - source
  *  properties:
- *      lessonInfo:
+ *      lesson_content:
  *          type: array
  *          items:
+ *            type: object
  *            schema:
  *              $ref: "#/definitions/VerseInformation"
  *          description: collection of complete verse Information for verses assocaited with a lesson
- *          example: [{
- *                      verse: {
- *                          "verse_index": 1,
-                            "surah": 1,
-                            "verse_number": 1,
-                            "verse_text": "بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ"},
-                        reflectons:{
-                            "reflection_id": 1,
-                            "verse_id": 1,
-                            "title": "Bismillah",
-                            "reflection": "My First Reflection"},
-                        tafsirs:{"tafsir_id": 1,
-                            "content": "In the name of Allah, The Most Gracious, The Most Merciful",
-                            "book": 1,
-                            "verse_id": 1,
-                            "visible": false,
-                            "verse_index": 1,
-                            "surah": 1,
-                            "verse_number": 1,
-                            "verse_text": "بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ"},
-                        words:{"word": "بِسْمِ",
-                            "root_id": 1,
-                            "word_explaination": "An explanation of the basmalla goes here.",
-                            "visible": false,
-                            "root_word": "س م و",
-                            "meaning": "to be high",
-                            "word_id": 1}
-                    }]
- *      lesson:
- *          type: object
- *          items: 
- *             schema: 
- *               $ref: "#/definitions/Lesson"
- *          description: lesson details and information
- *          example: {"lesson_id":1,"lesson_date":"2021-10-30T00:00:00.000Z","start_verse":1,"end_verse":2,"source":"https://google.com"} 
+ *      lesson_id:
+ *          type: integer
+ *          description: to identify the lesson from others
+ *          example: 1
+ *      lesson_date:
+ *          type: date
+ *          description: to identify the day that the lesson was taught
+ *          example: 2021-10-30
+ *      start_verse:
+ *          type: integer
+ *          description: first verse in a lesson
+ *          example: 1
+ *      end_verse:
+ *          type: integer
+ *          description: last verse in a lesson
+ *          example: 3
+ *      source:
+ *          type: string
+ *          description: a URL to the lesson recording
+ *          example: "https://www.facebook.com/watch/live/?ref=watch_permalink&v=244235014324418"
  */
 async function createLesson(data) {
     // Frontend note: also add a feature where we guess that the
@@ -95,17 +85,21 @@ async function createLesson(data) {
     var invalid = utils.simpleValidation(data, {
         lesson_date: "date",
         source: "string",
+        surah_id: "string",
+        start_verse: "integer",
+        surah_id: "integer",
     });
     if (invalid) {
         return invalid;
     }
-
-    let start = data.start_verse ? data.start_verse : null;
-    let end = data.end_verse ? data.end_verse : null;
-
     var sql =
         "INSERT INTO Lesson (source, lesson_date, start_verse, end_verse) VALUES ($1, $2, $3, $4) RETURNING *;";
-    var params = [data.source, data.lesson_date, start, end];
+    var params = [
+        data.source,
+        data.lesson_date,
+        data.start_verse,
+        data.end_verse,
+    ];
     return await utils.create(
         sql,
         params,
@@ -190,33 +184,36 @@ async function getLessonById(data) {
 /*Fetches verses in a lesson based*/
 //ASSUMPTIONS: Verses in a Lesson are contigous
 async function getLessonVerses(data) {
-    let msg = "";
     let lesson = await getLessonById(data);
     if (!lesson.success) {
         return lesson;
     }
 
-    let lessonInfo = [];
-    if (lesson.data[0].start_verse == null || lesson.data[0].end_verse == null) {
-        msg = "Invalid start or end verse value"
-    } else {
-        let currentVerse = lesson.data[0].start_verse;
-        let numVerses = lesson.data[0].end_verse - lesson.data[0].start_verse;
-        for (let i = 0; i <= numVerses; i++) {
-            let temp = await verseInfo.getVerseInfo({ verse_id: currentVerse })
-            lessonInfo[i] = temp.data;
-            currentVerse++;
+    let lesson_content = [];
+    let errors = [];
+    let currentVerse = lesson.data[0].start_verse;
+    let num_verses = lesson.data[0].end_verse - lesson.data[0].start_verse;
+    for (let i = 0; i <= num_verses; i++) {
+        let temp = await verseInfo.getVerseInfo({ verse_id: currentVerse });
+        lesson_content[i] = temp.data;
+        if (temp.ecode != utils.errorEnum.NONE) {
+            errors.push(
+                `Error on verse with id ${temp.data.verse_index}: ${temp.error}`
+            );
         }
-        msg = "Successfully fetched complete lesson info"
+        currentVerse++;
+    }
+    if (errors.length == 0) {
+        errors = "Successfully fetched complete lesson info";
     }
 
     let res = utils.setResult(
-        { lesson: lesson.data, lessonInfo },
+        { ...lesson.data[0], num_verses, lesson_content },
         lesson.success,
-        msg,
+        errors,
         lesson.ecode
     );
-    return res
+    return res;
 }
 
 /** Update a lesson, requires all attributes of the lesson. */
@@ -225,12 +222,21 @@ async function updateLesson(data) {
         lesson_id: "integer",
         lesson_date: "date",
         source: "string",
+        start_verse: "integer",
+        end_verse: "integer",
     });
     if (invalid) {
         return invalid;
     }
-    let sql = "UPDATE Lesson SET source=$2, lesson_date=$3 WHERE lesson_id=$1";
-    var params = [data.lesson_id, data.source, data.lesson_date];
+    let sql =
+        "UPDATE Lesson SET source=$2, lesson_date=$3, start_verse=$4, end_verse=$5 WHERE lesson_id=$1";
+    var params = [
+        data.lesson_id,
+        data.source,
+        data.lesson_date,
+        data.start_verse,
+        data.end_verse,
+    ];
     return await utils.update(
         sql,
         params,
@@ -267,5 +273,5 @@ module.exports = {
     createLesson,
     updateLesson,
     deleteLesson,
-    getLessonVerses
+    getLessonVerses,
 };
