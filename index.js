428d703b99ff9f9876cb849a03e3b38e28c1d783
ibrahim681 <81816993+ibrahim681@users.noreply.github.com>
2023-02-20 00:00:33 -0500

    commented out csrf

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(csrfMiddleware);
+// app.use(csrfMiddleware);
 
 app.engine("html", require("ejs").renderFile);
 
